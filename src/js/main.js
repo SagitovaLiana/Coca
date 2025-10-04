e67d48805277029f35957ca86a4ceb17e6149dc0
Sagitova <sagitova.liana.il@gmail.com>
2025-10-04 11:12:16 +0300

    Created header

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import '../scss/main.scss';
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+useTheme();
+useBurger();
--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
       input: {
         main: './src/index.html',
       },
+    },
   },
   plugins: [
     injectHTML(),
