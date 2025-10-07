8cb8fca1901f034a520e2178b4db8b6058834cd0
Sagitova <sagitova.liana.il@gmail.com>
2025-10-07 12:19:23 +0300

    Created pricing:hero section

---
+++ src/js/pricing.js
@@ -0,0 +1,8 @@
+import '../scss/pricing.scss';
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { usePriceSwitcher } from './components/priceSwitcher.js';
+
+useTheme();
+useBurger();
+usePriceSwitcher();
