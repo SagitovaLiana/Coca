f91427dc7839391900193b8c702320c739fc5c62
Sagitova <sagitova.liana.il@gmail.com>
2025-10-08 07:27:48 +0300

    Created contact:get-started section

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,6 +1,8 @@
 import '../scss/contact.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { usePhone } from './components/phone.js';
 
 useTheme();
 useBurger();
+usePhone();
